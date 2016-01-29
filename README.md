7649de609a4128e9458d66a5b90ddbdb2143219d
Kyle Mahan <kyle.mahan@gmail.com>
2016-01-28 20:10:28 -0800

    initial revision

---
+++ README.md
@@ -0,0 +1,9 @@
+# brevity-php
+
+[![Build Status](https://travis-ci.org/kylewm/brevity-php.svg?branch=master)](https://travis-ci.org/kylewm/brevity-php)
+
+A small utility to count characters, autolink, and shorten posts to an
+acceptable tweet-length summary.
+
+This is a port of the Python module of the same name. Please refer to
+https://github.com/kylewm/brevity for documentation.
---
+++ tests/BrevityTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace Kylewm;
+
+class BrevityTest extends \PHPUnit_Framework_TestCase
+{
+
+    private static function getTestData()
+    {
+        $testdata = json_decode(
+            file_get_contents(__DIR__ . '/../tests.json'), true);
+        return $testdata;
+    }
+
+
+    function shortenProvider()
+    {
+        $testdata = self::getTestData();
+        $result = [];
+        foreach ($testdata['shorten'] as $testcase) {
+            $result[] = [$testcase];
+        }
+        return $result;
+    }
+
+    /**
+     * @dataProvider shortenProvider
+     */
+    function testShorten($testcase)
+    {
+        $brevity = new Brevity();
+
+        if (isset($testcase['target_length'])) {
+            $brevity->setTargetLength($testcase['target_length']);
+        }
+        if (isset($testcase['link_length'])) {
+            $brevity->setLinkLength($testcase['link_length']);
+        }
+
+
+        $result = $brevity->shorten(
+            $testcase['text'],
+            isset($testcase['permalink']) ? $testcase['permalink'] : false,
+            isset($testcase['permashortlink']) ? $testcase['permashortlink'] : false,
+            isset($testcase['permashortcitation']) ? $testcase['permashortcitation'] : false,
+            isset($testcase['format_as_title']) ? $testcase['format_as_title'] : false);
+        $this->assertEquals($testcase['expected'], $result);
+    }
+
+    function autolinkProvider()
+    {
+        $testdata = self::getTestData();
+        return array_map(
+            function ($testcase) { return [$testcase]; },
+            $testdata['autolink']);
+    }
+
+    /**
+     * @dataProvider autolinkProvider
+     */
+    function testAutolink($testcase)
+    {
+        $brevity = new Brevity();
+        $result = $brevity->autolink($testcase['text']);
+        $this->assertEquals($testcase['expected'], $result);
+    }
+
+}
